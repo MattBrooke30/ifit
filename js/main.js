351bebf283fa8ba672a11004f2517a7ac28e735d
Matt Brooke <matt@thirdandgrove.com>
2017-02-15 23:16:43 -0500

    Add animations.

--- js/main.js
+++ js/main.js
@@ -41,4 +41,24 @@ $(document).ready(function() {
         prevNextButtons: false,
     });
 
+    // Fade and slide in hero text on page load
+    $('.js-hero-text').addClass('ready');
+
+    var gridWaypoint = new Waypoint({
+        element: $('.flex-grid'),
+        handler: function(direction) {
+            if (direction === 'down') {
+                $('.js-flex-item').addClass('fade-in');
+            }
+        },
+        offset: '75%'
+    });
+
+    // Basic parallax effect on hero iamge and text. No jQuery plugins needed :)
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        var imgPos = scrollTop / 4 + 'px';
+        $('.hero').find('img').css('transform', 'translateY(' + imgPos + ')');
+    });
+
 });
